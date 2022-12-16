311153569c1083a62d57a3b96a3de4b86f66354c
JeanGautierSherpas <jean@sherpas.com>
2022-12-16 23:38:10 +0100

    add svelte native type for stories

---
+++ src/components/Button/Button.stories.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+  
+  import Button from './Button.svelte';
+
+</script>
+
+<Meta
+  title="Design System/Button"
+  component={Button}
+/>
+
+<Template let:args>
+  <Button {...args} />
+</Template>
--- src/components/Button/Button.svelte
+++ src/components/Button/Button.svelte
@@ -1,3 +1,10 @@
+<script>
+    export let label = 'Button';
+</script>
+
+
 <button class="bg-sky-500 font-normal text-center py-4 px-7 text-white rounded-xl border-2 border-transparent relative text-lg hover:bg-sky-400 hover:cursor-pointer">
-    toto
+    <slot name="label">
+        {label}
+    </slot>
 </button>
