b096637a8fb9e121292c1c90357a683cf0ae589b
JeanGautierSherpas <jean@sherpas.com>
2022-12-21 09:40:05 +0100

    add ts on all component and format it

--- src/components/box-with-title/box-with-title.svelte
+++ src/components/box-with-title/box-with-title.svelte
@@ -1,12 +1,10 @@
-<script>
-  let title = 'Title';
-  
+<script lang="ts">
+	let title = 'Title';
 </script>
 
-
-  <div>
-    <div class="bg-white z-10 shadow-custom p-8 relative overflow-hidden mb-8 rounded-2xl">
-      <h5 class="mb-8 font-semibold relative z-10">{title}</h5>
-      <slot />
-    </div>
-  </div>
+<div>
+	<div class="bg-white z-10 shadow-custom p-8 relative overflow-hidden mb-8 rounded-2xl">
+		<h5 class="mb-8 font-semibold relative z-10">{title}</h5>
+		<slot />
+	</div>
+</div>
--- src/components/bread-crumb/bread-crumb.svelte
+++ src/components/bread-crumb/bread-crumb.svelte
@@ -1,11 +1,3 @@
-<script>
-
-</script>
-
-
-  <ul class="flex justify-center mt-4 sm:flex-row list-none p-0 m-0">
-    <slot />
-  </ul>
-
-
-
+<ul class="flex justify-center mt-4 sm:flex-row list-none p-0 m-0">
+	<slot />
+</ul>
